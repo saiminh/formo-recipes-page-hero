aef551f647f35ce780454669aa589a355815bb4a
saiminh <saiminh@gmail.com>
2024-10-14 13:23:41 +0200

    text zindex for editor

--- build/parent/index.asset.php
+++ build/parent/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '7e30455e238b86701791');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '61d0d0fdee11038f9d7f');
